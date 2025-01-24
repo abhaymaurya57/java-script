caa4f8a2817b560a0e96a41235bcf42247a98725
Your Name <abh.mrya@gmail.com>
2025-01-25 01:16:18 +0530

    variable

---
+++ 04_variable.js
@@ -0,0 +1,37 @@
+let value  =3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2= " abhay"
+
+let str3= str1 + str2
+console.log(str3)
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + "2");
+// console.log("1" + 2 +7);
+// console.log(5 + 7 + 2+ 2);
+// console.log((3*4+5)*2+5-5*5*43);
+console.log(true)
+console.log(+true)
+console.log(true+true)
+
+// let num1,num2,num3
+// num1=num2=num3=2+3
+// console.log(num1)
+// console.log(num2)
+// console.log(num3)
+let gamecounter = 100;
+gamecounter++;
+--gamecounter;
+--gamecounter;
+console.log(gamecounter)
