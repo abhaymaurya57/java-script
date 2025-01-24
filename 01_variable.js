46dfb2f39406ea9eca937b30221bed93b501882f
Your Name <abh.mrya@gmail.com>
2025-01-24 22:54:20 +0530

    variable

---
+++ 01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 101
+let accountemail = "abh.mrya1@gmail.com"
+var accpassword =    "#@$#@42"
+acountcity = "mumbai"
+let accountstate
+
+// accountId = 2
+accountemail="Fsfasf32.com"
+accpassword="4242"
+acountcity = "fdsf"
+console.log(accountId);
+console.table([accountId,accountemail,accpassword,acountcity,accountstate])
---
+++ 02_variable.js
@@ -0,0 +1,18 @@
+"use strict"; // treat al js code as never version
+// alert(3+3) we are using nodejs,not browser
+console.log(3+3)
+console.log("Abhay")
+let name = "kumar"
+let age=28
+let isLoggedIn = false
+let satate;
+// number => 2 to power 53
+// bigint
+// strinf =>""
+//boolean =>true/false
+//null undefine=>
+// symbol=>unique
+// oblect 
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof undifined);
