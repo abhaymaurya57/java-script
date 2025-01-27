a3be1315a9ce9cf9a32b9d098bb0d16b83210fd3
Your Name <abh.mrya@gmail.com>
2025-01-27 11:10:46 +0530

    new file

---
+++ 12_object.js
@@ -0,0 +1,8 @@
+const course = {
+    coursename: "js in hindi ",
+    price:"999",
+    courceInstructor:"Abhay"
+}
+//course.courceInstructor
+//const{courceInstructor}=course
+console.log(courceInstructor)
