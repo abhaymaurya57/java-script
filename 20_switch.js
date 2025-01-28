12b74fc0b34fcd9b4526f9037ef64e56d2d86b32
Your Name <abh.mrya@gmail.com>
2025-01-28 23:44:03 +0530

    new commit file

---
+++ 20_switch.js
@@ -0,0 +1,28 @@
+// switch(key){
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+switch (month) {
+    case "january":
+        console.log("january");
+        break;
+    case "feb" :
+        console.log("fab");
+        break;
+    case "march" :
+        console.log("march");
+       break;
+    case "aprile" :
+        console.log("april");
+        break;
+    
+
+    default:
+        console.log("defalt case match")
+        break;
+}
