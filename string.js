7a342668b5ab6d576953ffdaa434a7d325db13b4
Your Name <abhaymaurya54321@gmail.com>
2025-02-04 00:08:29 +0530

    commite

---
+++ string.js
@@ -0,0 +1,8 @@
+console.log('hello my name is ${name}');
+const gamename = new String('Abhaymaurya')
+console.log(gamename[0]);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.toLocaleLowerCase())
+console.log(gamename.charAt(5));
+console.log(gamename.indexOf('m'));
