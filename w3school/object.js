a2ec27b288ffb24963511ef2aa479416e7539a76
Abhay Kumar Maurya <abhaymaurya54321@gmail.com>
2025-02-08 15:14:58 +0530

    commit

---
+++ w3school/object.js
@@ -0,0 +1,9 @@
+// Create an Object
+const person = new Object();
+
+// Add Properties
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
+console.log(person)
---
+++ w3school/string.js
@@ -0,0 +1,9 @@
+let text = "John Doe";
+console.log(text)
+// Quotes Inside Quotes
+let answer3 = 'He is called "Johnny"';
+console.log(answer3);
+
+let text1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text1.length;
+console.log(length)
